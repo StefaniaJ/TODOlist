a639e07a77b156399d560eef109ba155636c1a66
Stefania <44357465+StefaniaJ@users.noreply.github.com>
2019-09-28 01:51:01 +0200

    function post done

--- script.js
+++ script.js
@@ -16,3 +16,25 @@ function get() {
     .then(e => console.log(e));
 }
 get();
+
+function post() {
+  const data = {
+    task: "drink more water",
+    date: "2019-09-27T23:50:31.030Z",
+    important: true
+  };
+
+  const postData = JSON.stringify(data);
+  fetch(link, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": corskey,
+      "cache-control": "no-cache"
+    },
+    body: postData
+  })
+    .then(res => res.json())
+    .then(data => console.log(data));
+}
+post();
