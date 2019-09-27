f05f8c822d60553d7d88856f96badb593ad33e59
Stefania <44357465+StefaniaJ@users.noreply.github.com>
2019-09-28 01:46:41 +0200

    function get done

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 
 const link = "https://singers-373b.restdb.io/rest/todo";
 const corskey = "5d8a48a2fd86cb75861e26eb";
+
+function get() {
+  fetch("https://singers-373b.restdb.io/rest/todo", {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": "5d8a48a2fd86cb75861e26eb",
+      "cache-control": "no-cache"
+    }
+  })
+    .then(e => e.json())
+    .then(e => console.log(e));
+}
+get();
