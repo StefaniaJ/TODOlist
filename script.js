a73c9c37f408b8f5d12877a6b5ce2131fc4a0a35
Stefania <44357465+StefaniaJ@users.noreply.github.com>
2019-09-28 02:37:18 +0200

    function deletId works in console

--- script.js
+++ script.js
@@ -20,18 +20,23 @@ function get() {
 }
 get();
 
-function addTasksToTheDOM(taskE) {
+function addTasksToTheDOM(task) {
   // Set the variables
   const template = document.querySelector("#template").content;
   const clone = template.cloneNode(true);
   const parent = document.querySelector(".toDoList");
 
   // Add content to the template
-  clone.querySelector(".taskName").textContent = taskE.task;
-  clone.querySelector(".time").textContent = taskE.time;
-  clone.querySelector(".day").textContent = taskE.day;
-  clone.querySelector(".date").textContent = taskE.date;
-  clone.querySelector(".important").textContent = taskE.important;
+  clone.querySelector(".taskName").textContent = task.task;
+  clone.querySelector(".time").textContent = task.time;
+  clone.querySelector(".day").textContent = task.day;
+  clone.querySelector(".date").textContent = task.date;
+  clone.querySelector(".important").textContent = task.important;
+
+  clone.querySelector(".btnRemoveTask").addEventListener("click", () => {
+    console.log(task._id);
+    deleteIt(task._id);
+  });
 
   // Append the clone to the parent
   parent.appendChild(clone);
@@ -57,4 +62,17 @@ function post() {
     .then(res => res.json())
     .then(data => console.log(data));
   addTasksToTheDOM(data);
+}
+
+function deleteIt(id) {
+  fetch("https://singers-373b.restdb.io/rest/todo/" + id, {
+    method: "delete",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": corskey,
+      "cache-control": "no-cache"
+    }
+  })
+    .then(res => res.json())
+    .then(data => console.log(data));
 }
